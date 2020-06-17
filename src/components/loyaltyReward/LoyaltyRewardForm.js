73ab169ebe0861b4ba8d57e6ef44e1b4f96cdb60
leigharobinson <lmarobinson24@gmail.com>
2020-06-16 19:59:54 -0500

    Fixed all the bugs for Form

--- src/components/loyaltyReward/LoyaltyRewardForm.js
+++ src/components/loyaltyReward/LoyaltyRewardForm.js
@@ -2,7 +2,14 @@ import React, { useState } from "react";
 import LoyaltyRewardManager from "../../modules/LoyaltyRewardManager";
 
 const LoyaltyRewardForm = (props) => {
-  const [loyaltyReward, setLoyaltyReward] = useState({ forLocation: "" });
+  const [loyaltyReward, setLoyaltyReward] = useState({
+    forLocation: "",
+    datetime: "",
+    visitsUntilReward: "",
+    expirationDate: "",
+    url: "",
+    notes: "",
+  });
   const [isLoading, setIsLoading] = useState(false);
 
   const handleFieldChange = (evt) => {
@@ -15,13 +22,20 @@ const LoyaltyRewardForm = (props) => {
    */
   const constructNewLoyaltyReward = (evt) => {
     evt.preventDefault();
-    if (loyaltyReward.forLocation === "") {
+    if (
+      !loyaltyReward.forLocation ||
+      !loyaltyReward.datetime ||
+      !loyaltyReward.visitsUntilReward ||
+      !loyaltyReward.expirationDate ||
+      !loyaltyReward.url ||
+      !loyaltyReward.notes
+    ) {
       window.alert("Please fill out entire form");
     } else {
       setIsLoading(true);
       // Create the animal and redirect user to animal list
       LoyaltyRewardManager.post(loyaltyReward).then(() =>
-        props.history.push("/loyaltyRewardsCardswards")
+        props.history.push("/loyaltyRewardsCards")
       );
     }
   };
@@ -39,6 +53,48 @@ const LoyaltyRewardForm = (props) => {
               placeholder="Business Name"
             />
             <label htmlFor="forLocation">Business Name</label>
+            <input
+              type="date"
+              required
+              onChange={handleFieldChange}
+              id="datetime"
+              placeholder="date"
+            />
+            <label htmlFor="datetime">date created</label>
+            <input
+              type="text"
+              required
+              onChange={handleFieldChange}
+              id="visitsUntilReward"
+              placeholder="visitsUntilReward"
+            />
+            <label htmlFor="visitsUntilReward">
+              Number of Visits Until Next Reward
+            </label>
+            <input
+              type="date"
+              required
+              onChange={handleFieldChange}
+              id="expirationDate"
+              placeholder="expiratioinDate"
+            />
+            <label htmlFor="expirationDate">Expiration Date</label>
+            <input
+              type="url"
+              required
+              onChange={handleFieldChange}
+              id="url"
+              placeholder="url"
+            />
+            <label htmlFor="url">Business's url</label>
+            <input
+              type="text"
+              required
+              onChange={handleFieldChange}
+              id="notes"
+              placeholder="notes"
+            />
+            <label htmlFor="notes">Notes</label>
           </div>
           <div className="alignRight">
             <button
